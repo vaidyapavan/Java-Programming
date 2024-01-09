7025ba8ce54c0513fd1baead6149a10ac4f5965e
LENOVO <LENOVO@DESKTOP-DJ211IC>
2024-01-10 00:11:32 +0530

    commit

---
+++ src/java/Absoluteno.java
@@ -0,0 +1,30 @@
+package java;
+
+class Absolutenumber
+{
+	void absolutenumber(int no)
+	{
+		if(no>0)
+		{
+			System.out.println("no is absolute");		
+			}
+		else
+		{
+			System.out.println("no is not absolute");	
+			
+		}
+	}
+	
+}
+
+public class Absoluteno {
+	public  static void main( String args[])
+	{
+		Absolutenumber a=new Absolutenumber();
+		a.absolutenumber(10);
+		
+		
+	}
+	
+
+}
