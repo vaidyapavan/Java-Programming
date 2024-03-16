f38a9fd30f7a0646177d110ceba3eda1f7ccc76e
pavan vaidya <vaidyapavan1000@gmail.com>
2024-03-16 19:11:42 +0530

    commit

---
+++ Basic_programs/src/DSA/Insertionsort.java
@@ -0,0 +1,21 @@
+package DSA;
+
+public class Insertionsort {
+
+    public static void main(String[] args) {
+        int arr[] = {20, 30, 90, 3, 1, 8, 40};
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+        // Print the sorted array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
