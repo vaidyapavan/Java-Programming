67019c8c05c5c193c2669b201ecb294a61d4b95e
pavan vaidya <vaidyapavan1000@gmail.com>
2024-03-18 22:01:00 +0530

    commit

---
+++ Basic_programs/src/Starpattern/Starpattern1.java
@@ -0,0 +1,20 @@
+package Starpattern;
+
+public class Starpattern1 {
+
+	public static void main(String[] args) {
+		int no=10;
+		for(int i=0;i<=no;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+			
+			
+		}
+
+	}
+
+}
