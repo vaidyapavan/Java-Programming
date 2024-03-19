c35f13edefb8a3e9942c436c08f977efd291477c
pavan vaidya <vaidyapavan1000@gmail.com>
2024-03-19 23:44:36 +0530

    commit

---
+++ Basic_programs/src/DSA/Oddeven.java
@@ -0,0 +1,26 @@
+package DSA;
+import java.util.*;
+
+public class Oddeven {
+
+	public static void main(String[] args) {
+		Scanner s =new Scanner(System.in);
+		int n;
+		System.out.println("Enter thne nmber:");
+		n=s.nextInt();
+		
+		
+		
+		if(n%2==0)
+		{
+			System.out.println("number   is  even");
+			}
+		else {
+			System.out.println("number   is  odd ");
+			
+		}
+		
+
+	}
+
+}
