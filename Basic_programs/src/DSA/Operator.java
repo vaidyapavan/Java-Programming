c79a88f5dea2df50695909d8684a05e741cd6a28
pavan vaidya <vaidyapavan1000@gmail.com>
2024-03-22 16:13:23 +0530

    commit

---
+++ Basic_programs/src/DSA/Operator.java
@@ -0,0 +1,20 @@
+package DSA;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=30;
+		
+		int c;
+//		c=a++; // post increament
+//		
+//		System.out.println(a);
+//		System.out.println(c);
+//		System.out.println(a);
+		c=++a;
+		System.out.println(a);
+
+	}
+
+}
