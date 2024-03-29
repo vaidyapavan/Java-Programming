64e45845a5d14e6cb2d0cb9d4b3f7c0303ef21f4
pavan vaidya <vaidyapavan1000@gmail.com>
2024-03-29 17:07:55 +0530

    commit

--- Basic_programs/src/Basic_java/Object_creation.java
+++ Basic_programs/src/Basic_java/Object_creation.java
@@ -1,10 +1,41 @@
 package Basic_java;
 
+import java.io.*;
+
+class Sports {
+    String getName() {
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers() {
+        System.out.println("Each team has n players in " + getName());
+    }
+}
+
+class Soccer extends Sports {
+    @Override
+    String getName() {
+        return "Soccer Class";
+    }
+
+    // Overriding the getNumberOfTeamMembers method
+    @Override
+    void getNumberOfTeamMembers() {
+        System.out.println("Each team has 11 players in " + getName());
+    }
+}
+
 public class Object_creation {
+    public static void main(String[] args) {
+        // Creating instances of Sports and Soccer classes
+        Sports sports = new Sports();
+        Soccer soccer = new Soccer();
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        // Printing results
+        System.out.println(sports.getName());
+        sports.getNumberOfTeamMembers();
 
-	}
-
+        System.out.println(soccer.getName());
+        soccer.getNumberOfTeamMembers();
+    }
 }
