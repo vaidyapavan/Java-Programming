be1bdfefbac43bb848fbcd5af6095e98ad874f85
pavan vaidya <vaidyapavan1000@gmail.com>
2024-04-07 00:41:05 +0530

    commit

---
+++ Basic_programs/src/DSA/Absolute_no.java
@@ -0,0 +1,27 @@
+package DSA;
+import java.util.*;
+public class Absolute_no {
+
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		System.out.println("enter the number:");
+		int n;
+		n= s.nextInt();
+		
+		if(n>=0)
+		{
+			System.out.println( n+" is absolute");
+			
+		}
+		else 
+		{
+			System.out.println( n+" is  not absolute");
+			
+		}
+		
+		
+		
+		
+	}
+
+}
