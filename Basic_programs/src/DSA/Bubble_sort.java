65a3e6024c40e5b73888b8366bc6b7aff7cbae4f
pavan vaidya <vaidyapavan1000@gmail.com>
2024-04-18 11:37:49 +0530

    commit

--- Basic_programs/src/DSA/Bubble_sort.java
+++ Basic_programs/src/DSA/Bubble_sort.java
@@ -5,6 +5,8 @@ class BubbleSort {
     int n = arr.length;
 
     void sort() {
+    	
+    	
         int temp;
         for (int i = 0; i < n - 1; i++) { // Corrected loop condition
             for (int j = 0; j < n - i - 1; j++) { // Corrected loop condition
